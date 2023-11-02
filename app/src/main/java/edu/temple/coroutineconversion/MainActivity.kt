a7d302f6f07cbfd295929c8fd17e368c445c2f02
Adrien Tolno <tus29878@temple.edu>
2023-11-02 15:09:09 -0400

    I create a scope function and give it as default and I create a new class name cake where it will delay to 40 time millis. The repeat time is 100.

--- app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
+++ app/src/main/java/edu/temple/coroutineconversion/MainActivity.kt
@@ -6,6 +6,12 @@ import android.os.Handler
 import android.os.Looper
 import android.widget.Button
 import android.widget.ImageView
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class MainActivity : AppCompatActivity() {
 
@@ -17,20 +23,25 @@ class MainActivity : AppCompatActivity() {
         cakeImageView.alpha = it.what / 100f
         true
     })
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         cakeImageView = findViewById(R.id.imageView)
 
+        val scope = CoroutineScope(Job() + Dispatchers.Default)
         findViewById<Button>(R.id.revealButton).setOnClickListener{
-            Thread{
-                repeat(100) {
-                    handler.sendEmptyMessage(it)
-                    Thread.sleep(40)
-                }
-            }.start()
+            scope.launch {
+            }
         }
     }
+    suspend fun Cake() {
+        repeat(100) {
+            withContext(Dispatchers.Main) {
+                handler.sendEmptyMessage(it)
+            }
+        }
+        delay(40)
+    }
 }
+
